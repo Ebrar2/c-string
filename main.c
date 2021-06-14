205c4e296c1f1354ece0003554ccf865f2a806cd
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 18:48:01 +0300

    iki stringi bölerek yapıştıma

--- main.c
+++ main.c
@@ -1,35 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<ctype.h>
+void yapitirnk(char *s1,int x,char *s2,int y)
+{
+    int i,j=0;
+    for(i=x-1;j!=y;i++)
+    {
+         s1[i]=s2[j];
+         j++;
+    }
+}
 int main()
 {
-    char i,j,k,y;
-    int x;
- /* for(i='A';(i-1)!='Z';i++)
-    {
-        printf("|%-3c %-5d|%-3c  %-5d|\n",i,i,i+32,i+32);
-    }
-    int x;
-    printf("\n--------------------------------------------------\n");
-    for(x=65;x<=90;x++)
-    {
-        printf("|%-3c %-5d|%-3c  %-5d|\n",x,x,x+32,x+32);
-    }*/
-    for(x=0;x<32;x++)
-    {
-        i=x;
-        if(isprint(i)==0)
-            i=' ';
-        j=x+32;
-        if(isprint(j)==0)
-            j=' ';
-        k=x+64;
-        if(isprint(k)==0)
-            k=' ';
-        y=x+96;
-        if(isprint(y)==0)
-            y=' ';
-       printf("|%-3c %4d|%-3c %4d|%-3c %4d|%-3c %4d|\n",i,i,j,j,k,k,y,y);
-    }
+    char s1[]="Bilgisayar Arasinav";
+    char s2[]="102 Dersi";
+    yapitirnk(s1,12,s2,3);
+    printf("%s\n",s1);
     return 0;
 }
