2a240ec7d54ec0a02cdb7af14ba1ac79ea62a8ed
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 19:01:08 +0300

    cümlede eğer 1 den fazla  arka arkayaboşluk varsa silme

--- main.c
+++ main.c
@@ -1,57 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
-void devrikYap(char *d)
+void fazlaBoslukSil(char *d)
 {
-   char *a,*b,*c,*e,gec[100],gec2[100];
-   int i;
-   strcpy(gec,d);
-   strcpy(gec2,d);
-   e=strstr(gec," ");
-   while(strstr(e+1," ")!=NULL)
-   {
-       e=strstr(e+1," ");
-    //   printf("\ne:%s e:%d gec:%d gec:%s",e,e,gec,gec);
-   }
-   a=strtok(gec2," ");
-   b=strtok(NULL," ");
-   c=strtok(NULL," ");
-  // a=strtok(gec3," ");
- //  printf("\n\na:%s\n",a);
-  // printf("b:%s\n",b);
-  // printf("c:%s\n",c);
-  // printf("e:%e\n",e);
-   strcpy(d,e);
-   for(i=0;d[i]!='\0';i++)
-   {
-
-   }
-   d[i]=' ';
-   d[i+1]='\0';
- //  printf("\nd:%s e:%s",d,e);
-   strcat(d,c);
-   for(i=0;d[i]!='\0';i++)
-   {
-
-   }
-   d[i]=' ';
-   d[i+1]='\0';
- //  printf("\nd:%s e:%s  c:%s",d,e,c);
-   strcat(d,b);
+    int i,j;
     for(i=0;d[i]!='\0';i++)
-   {
+    {
+        if(d[i]==' ' && d[i+1]==' ')
+        {
 
-   }
-   d[i]=' ';
-   d[i+1]='\0';
- //   printf("\nd:%s e:%s  c:%s  b:%s",d,e,c,b);
-   strcat(d,a);
-  // printf("\nd:%s e:%s  c:%s  b:%s a:%s",d,e,c,a);
+            for(j=i;d[j]!='\0';j++)
+            {
+                d[j]=d[j+1];
+            }
+            i=0;
+        }
+
+    }
 }
 int main()
 {
-    char d[100]="baharda agaclar kuslar guzeldir";
-     devrikYap(d);
-    printf("Devrik cumle:%s",d);
+    char d[50];
+    printf("Cumleyi giriniz:");
+    fgets(d,50,stdin);
+    fazlaBoslukSil(d);
+    printf("\nSon Hali:%s",d);
     return 0;
 }
