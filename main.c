11945ca69949ac8d08dd5801e21abc333a5d70d3
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 19:20:40 +0300

    10 tane ad not olarak girilmiş stringi nota göre sıralama

--- main.c
+++ main.c
@@ -1,57 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
-void emailBreak(char *d,int x,char *s)
-{
-    int i,j,y=0,k;
-    if(x==1)
-    {
-         for(i=0;d[i]!='@';i++)
-                s[i]=d[i];
-      s[i]='\0';
-    }
-    if(x==2)
-    {
-        for(i=0;d[i]!='@';i++)
-         {
-
-         }
-        for(j=i+1;d[j]!='.';j++)
-        {
-            s[y]=d[j];
-            y++;
-        }
-        s[j]='\0';
-    }
-    else if(x==3)
-    {
-         for(i=0;d[i]!='@';i++)
-         {
-
-         }
-         for(j=i+1;d[j]!='.';j++)
-         {
-
-         }
-        y=0;
-          for(k=j+1;d[k]!='\0';k++)
-        {
-            s[y]=d[k];
-            y++;
-        }
-        s[k]='\0';
-    }
-
-  //    printf("\n%s\n",s);
-
-}
+#include<ctype.h>
 int main()
 {
-    char d[]="ebrar.temel@hotmail.com";
-    char s1[50],s2[50],s3[50];
-    emailBreak(d,1,s1);
-    emailBreak(d,2,s2);
-    emailBreak(d,3,s3);
-    printf("\n%s-%s-%s\n",s1,s2,s3);
+     char d[10][10],*k,gec[10][10];
+     int e[10]={0};
+     int i,j,z;
+     printf("Isim ve notlari giriniz:\n");
+     for(i=0;i<10;i++)
+     {
+        scanf ("%[^\n]", d[i]);
+        fflush(stdin);
+     }
+     for(i=0;i<10;i++)
+     {
+         k=strstr(d+i," ");
+         e[i]=atoi(k+1);
+     }
+
+     for(i=0;i<10;i++)
+     {
+         for(j=i+1;j<10;j++)
+         {
+             if(e[i]<e[j])
+             {
+                 int gec2;
+                 gec2=e[i];
+                 e[i]=e[j];
+                 e[j]=gec2;
+                 strcpy(gec,d[i]);
+                 //printf("\ngec:%s d[i]:%s d[j]:%s",gec,d[i],d[j]);
+                 strcpy(d[i],d[j]);
+                 strcpy(d[j],gec);
+                 //printf("\ngec:%s d[i]:%s d[j]:%s",gec,d[i],d[j]);
+
+             }
+         }
+     }
+     printf("\n\nNota gore sirala\n\n");
+     for(i=0;i<10;i++)
+        printf("%s\n",d[i]);
     return 0;
 }
