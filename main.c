abd392bff1ba6273e97e31bbf00568634a6e4914
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 19:26:36 +0300

    istenilen kelimeleri sansürleme

--- main.c
+++ main.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
+void sansur(char d[])
+{
+    char *p,a[3][10]={"lan","inek","aptal"};
+    int i,j,x;
+    for(i=0;d[i]!='\0';i++)
+    {
+        for(x=0;x<3;x++)
+          {
+              printf("\nx:%d p:%d d:%d p-d:%d",x,p,d,p-d);
+              p=strstr(d+i,a[x]);
+              if(p!=NULL)
+              {
+                  printf("j p-d:%d\n",p-d);
+                  for(j=p-d;j<p-d+strlen(a[x]);j++)
+                   {
+                       d[j]='*';
+                       printf("j:%d\n",j);
+                   }
+              }
+          }
+    }
+}
 int main()
 {
-    char d[10][20];
-    int i,a;
-    printf("Isimleri giriniz:\n");
-    for(i=0;i<10;i++)
-     {
-         scanf("%s",d[i]);
-         printf("\n");
-     }
-    printf("an ile bitenler:\n");
-    for(i=0;i<10;i++)
-    {
-        a=strlen(d[i]);
-        if(d[i][a-2]=='a'  &&  d[i][a-1]=='n')
-            printf("%s\n",d[i]);
-    }
+    char d[200];
+    printf("Cumleyi giriniz:\n");
+    fgets(d,200,stdin);
+    sansur(d);
+    printf("\n\nYeni Hal\n");
+    printf("%s\n",d);
     return 0;
 }
