92cf37d1066ab69fb4b400dcdf9bdf561bec41bd
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 18:41:43 +0300

    5 ders kodunun b ile başlayanları

--- main.c
+++ main.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+
 int main()
 {
-    char d[5][20];
-    char gec[10];
-    int i,j,k;
-    printf("Isimleri giriniz:\n");
+    char kod[5][20];
+    int i;
+    printf("\nDers kodlarini giriniz:");
+    for(i=0;i<5;i++)
+    {
+        printf("\n%d. dersin kodu:",i+1);
+        scanf("%s",&kod[i][0]);
+        //scanf("%s",kod[i]);
+    }
+    printf("\nB harfiyle baslayanlar:\n");
+      for(i=0;i<5;i++)
+    {
+        if(kod[i][0]=='B')
+            printf("%s,",kod[i]);
 
-    for(i=0;i<5;i++)
-    {
-        scanf("%s",d[i]);
-        printf("\n");
     }
-    for(i=0;i<5;i++)
-    {
-        for(j=0;j<4;j++)
-        {
-            k=strcmp(d[j],d[j+1]);
-            if(k>0)
-            {
-                strcpy(gec,d[j]);
-                strcpy(d[j],d[j+1]);
-                strcpy(d[j+1],gec);
-            }
-        }
-    }
-    printf("\nSiralanmis Hali:\n");
-    for(i=0;i<5;i++)
-        printf("%s\n",d[i]);
     return 0;
 }
