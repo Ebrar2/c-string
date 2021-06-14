ad269e8ae07e290613dfa3faa6f18e54ff7117c5
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 18:49:45 +0300

    dizginin büyük ünlü uyumuna uyp uymadığını kontrol etme

--- main.c
+++ main.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-void spacemask(char *d)
-{
-    int i;
-    for(i=0;d[i]!='\0';i++)
-    {
-        if(d[i]==' ')
-            d[i]='_';
-    }
-}
+
 int main()
 {
-    char sample[]="This is a test";
-    printf("Once:\n");
-    printf("%s",sample);
-    spacemask(sample);
-    printf("\nSonra:\n");
-    printf("%s",sample);
+    char d[100];
+    int x=0,y=0,i;
+    printf("Dizgiyi giriniz:");
+    fgets(d,100,stdin);
+    for(i=0;d[i]!='\0';i++)
+    {
+        if(d[i]=='a' || d[i]=='ý' || d[i]=='o' || d[i]=='u')
+            x++;
+        else if(d[i]=='e' || d[i]=='i' || d[i]=='ö' || d[i]=='ü')
+            y++;
+    }
+    if(x==0 && y!=0)
+        printf("\nBuyu unlu uyumuna uyar");
+    else if(x!=0 && y==0)
+        printf("\nBuyu unlu uyumuna uyar");
+    else
+        printf("\nBuyuk unlu uyumuna uymaz");
     return 0;
 }
