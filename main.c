3c8eff376a12a2e3b2a3d14868a4d4aa99d97354
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 18:55:26 +0300

    cümle içinden kelime çıkarma

--- main.c
+++ main.c
@@ -1,52 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<ctype.h>
-#include<string.h>
-int cevir(char x);
-int rakamTopla(char *d)
+void bulCikar(char *d,char *c)
 {
-    int top=0,i,x,gec,say;
-    for(i=0;d[i]!='\0';i++)
+    char *p;
+    int i,say;
+    say=strlen(c);
+    p=strstr(d,c);
+    for(i=p-d;d[i]!='\0';i++)
+        d[i]=d[i+say];
+  //    printf("%s\n",d);
+    while(p!=d+strlen(d))
     {
-        if(isdigit(d[i]))
-        {
-            say=cevir(d[i]);
-            top=top+say;
-        }
+        p=strstr(d,c);
+      for(i=p-d;d[i]!='\0';i++)
+           d[i]=d[i+say];
+   //   printf("%s  d:%d p:%d   p:%s \n",d,d,p,p);
     }
-    return top;
-}
-int cevir(char x)
-{
-    int a;
-    a=(int)x;
-   // printf("\na:%d x:",a,x);
-    if(a==48)
-        return 0;
-    else if(a==49)
-        return 1;
-    else if(a==50)
-        return 2;
-    else if(a==51)
-        return 3;
-    else if(a==52)
-        return 4;
-    else if(a==53)
-        return 5;
-    else if(a==54)
-        return 6;
-    else if(a==55)
-        return 7;
-    else if(a==56)
-        return 8;
-    else if(a==57)
-        return 9;
+
 }
 int main()
 {
-    char d[100];
-    printf("Cumleyi giriniz:");
-    fgets(d,100,stdin);
-    printf("Cumledeki rakamlarin toplami:%d",rakamTopla(d));
+    char d[]="Kamuran  muratla aysenin yaninda toplumur";
+    char c[]="mur";
+    bulCikar(d,c);
+      printf("%s\n",d);
     return 0;
 }
