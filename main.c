d055f2b09f65ce8561609921c2a3b9b1937d49c9
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 18:46:38 +0300

    ASCII tablosu yazdırma

--- main.c
+++ main.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include<ctype.h>
 int main()
 {
-    char d[4][50];
-    int i;
-    printf("Siirinizi giriniz:\n");
-    for(i=0;i<4;i++)
+    char i,j,k,y;
+    int x;
+ /* for(i='A';(i-1)!='Z';i++)
     {
-        scanf("%[^\n]",d[i]);
-        fflush(stdin);
-        printf("\n");
+        printf("|%-3c %-5d|%-3c  %-5d|\n",i,i,i+32,i+32);
     }
-    for(i=0;i<4;i++)
-       {
-           printf("%c",d[i][0]);
-       }
+    int x;
+    printf("\n--------------------------------------------------\n");
+    for(x=65;x<=90;x++)
+    {
+        printf("|%-3c %-5d|%-3c  %-5d|\n",x,x,x+32,x+32);
+    }*/
+    for(x=0;x<32;x++)
+    {
+        i=x;
+        if(isprint(i)==0)
+            i=' ';
+        j=x+32;
+        if(isprint(j)==0)
+            j=' ';
+        k=x+64;
+        if(isprint(k)==0)
+            k=' ';
+        y=x+96;
+        if(isprint(y)==0)
+            y=' ';
+       printf("|%-3c %4d|%-3c %4d|%-3c %4d|%-3c %4d|\n",i,i,j,j,k,k,y,y);
+    }
     return 0;
 }
