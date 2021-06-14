a1dc7bb48d951a65fb0c4864e719b51d13ad3969
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 18:34:23 +0300

    Palindrom dizgimi

--- main.c
+++ main.c
@@ -1,40 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
-int sonundaVarmi(char *s1,char *s2)
-{
-    char *k;
-    char d[strlen(s1)+1];
-    strcpy(d,s1);
-   // printf("%s\n",d);
-    int i,a=0;
-    k=strtok(s1," ");
-   // printf("%s\n",k);
-    while(k!=NULL)
-    {
-        a++;
-        k=strtok(NULL," ");
-     //   printf("%s\n",k);
-     //   printf("\na:%d\n",a);
-    }
-    k=strtok(d," ");
-    while(a!=1)
-    {
-        a--;
-        k=strtok(NULL," ");
-       // printf("\n%s  a:%d",k,a);
-
-    }
-    k[strlen(s2)]='\0';
-    if(strcmp(k,s2)==0)
-        return 1;
-    else
-        return 0;
-}
 int main()
 {
-    char s1[]="A bulbul neden kondun dalimdaki asmaya,";
-    char s2[]="asmaya";
-    printf("%d",sonundaVarmi(s1,s2));
+   char d[20];
+   int i,a,say=0;
+   printf("Dizgiyi giriniz:");
+   fgets(d,20,stdin);
+   a=strlen(d)-1;
+   for(i=0;i<a/2;i++)
+   {
+       if(d[i]==d[a-1-i])
+        say++;
+   }
+  if(say==a/2)
+     printf("Palindrom dizgi");
+  else
+      printf("Palindrom olmayan dizgi");
     return 0;
 }
