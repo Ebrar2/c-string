e031767b0b4e1a23038db192a2278d7bcc26f968
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 18:58:47 +0300

    kurallı cümleyi devrik yapma

--- main.c
+++ main.c
@@ -1,28 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-void bulCikar(char *d,char *c)
+#include<string.h>
+void devrikYap(char *d)
 {
-    char *p;
-    int i,say;
-    say=strlen(c);
-    p=strstr(d,c);
-    for(i=p-d;d[i]!='\0';i++)
-        d[i]=d[i+say];
-  //    printf("%s\n",d);
-    while(p!=d+strlen(d))
-    {
-        p=strstr(d,c);
-      for(i=p-d;d[i]!='\0';i++)
-           d[i]=d[i+say];
-   //   printf("%s  d:%d p:%d   p:%s \n",d,d,p,p);
-    }
+   char *a,*b,*c,*e,gec[100],gec2[100];
+   int i;
+   strcpy(gec,d);
+   strcpy(gec2,d);
+   e=strstr(gec," ");
+   while(strstr(e+1," ")!=NULL)
+   {
+       e=strstr(e+1," ");
+    //   printf("\ne:%s e:%d gec:%d gec:%s",e,e,gec,gec);
+   }
+   a=strtok(gec2," ");
+   b=strtok(NULL," ");
+   c=strtok(NULL," ");
+  // a=strtok(gec3," ");
+ //  printf("\n\na:%s\n",a);
+  // printf("b:%s\n",b);
+  // printf("c:%s\n",c);
+  // printf("e:%e\n",e);
+   strcpy(d,e);
+   for(i=0;d[i]!='\0';i++)
+   {
 
+   }
+   d[i]=' ';
+   d[i+1]='\0';
+ //  printf("\nd:%s e:%s",d,e);
+   strcat(d,c);
+   for(i=0;d[i]!='\0';i++)
+   {
+
+   }
+   d[i]=' ';
+   d[i+1]='\0';
+ //  printf("\nd:%s e:%s  c:%s",d,e,c);
+   strcat(d,b);
+    for(i=0;d[i]!='\0';i++)
+   {
+
+   }
+   d[i]=' ';
+   d[i+1]='\0';
+ //   printf("\nd:%s e:%s  c:%s  b:%s",d,e,c,b);
+   strcat(d,a);
+  // printf("\nd:%s e:%s  c:%s  b:%s a:%s",d,e,c,a);
 }
 int main()
 {
-    char d[]="Kamuran  muratla aysenin yaninda toplumur";
-    char c[]="mur";
-    bulCikar(d,c);
-      printf("%s\n",d);
+    char d[100]="baharda agaclar kuslar guzeldir";
+     devrikYap(d);
+    printf("Devrik cumle:%s",d);
     return 0;
 }
