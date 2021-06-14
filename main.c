50ad5c839fa97734eec90f9ab5427351c85c8939
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 19:31:52 +0300

    pangram cümlemi kontrolü
    alfabedeki tüm harfleri barındıran cümlelere pangram denir

--- main.c
+++ main.c
@@ -1,43 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
-#include<string.h>
+#include<ctype.h>
+int hesapla(char *d,int *harfS)
+{
+    int i,j,say=0,r,l;
+    char c[26]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int a[26]={0};
+    for(i=0;d[i]!='\0';i++)
+    {
+        if(isalpha(d[i]))
+        {
+          for(j=0;j<26;j++)
+        {
+            r=d[i];
+            l=c[j];
+            if(d[i]==c[j] || r==l+32)
+                a[j]=1;
+         // printf("\nr:%d l:%d d[%d]:%c c[%d]:%c a[%d]:%d",r,l,i,d[i],j,c[j],j,a[j]);
+
+         }
+        }
+
+    }
+    for(i=0;i<26;i++)
+    {
+        if(a[i]!=0)
+            say++;
+        *harfS=say;
+  //  printf("\nharf:%d  say:%d",*harfS,say);
+    }
+    for(i=0;i<26;i++)
+    {
+        if(a[i]==0)
+            return 0;
+
+    }
+    return 1;
+}
 int main()
 {
-    char sifat[5][20]={"Bu","Su","O","Bir","Birkac"};
-    char ozne[5][20]={"adam","cocuk","kus","kedi","araba"};
-    char tumlec[5][20]={"sinif","kapan","orman","cayir","agac"};
-    char tumlecek[5][20]={"a","dan","in ustunden","in altindan","in icinden"};
-    char yuklem[5][20]={"atladi.","kacti.","zipladi.","kostu.","dustu."};
-    char d[40];
-    srand(time(NULL));
-    int x,a=0;
-   while(a!=5)
-    {
-      a++;
-      int b;
-    b=strlen(d);
-    x=rand()%5;
-    strcpy(d,sifat[x]);
-    b=strlen(d);
-    d[b]=' ';
-    d[b+1]='\0';
-    x=rand()%5;
-    strcat(d,ozne[x]);
-    b=strlen(d);
-    d[b]=' ';
-    d[b+1]='\0';
-    x=rand()%5;
-    strcat(d,tumlec[x]);
-    x=rand()%5;
-    strcat(d,tumlecek[x]);
-    x=rand()%5;
-    b=strlen(d);
-    d[b]=' ';
-    d[b+1]='\0';
-    strcat(d,yuklem[x]);
-    printf("\n\n\nCumle:\n");
-    printf("%s",d);
-    }
+    char d[60];
+    int harfS;
+    printf("Ingilizce cumleyi giriniz:");
+    fgets(d,60,stdin);
+    if(hesapla(d,&harfS))
+         printf("\n\nBu cumle palindromdur");
+    else
+        printf("\n\nBu cumle palindrom degildir");
+    printf("\nBu cumledeki harf sayisi:%d",harfS+1);
     return 0;
 }
